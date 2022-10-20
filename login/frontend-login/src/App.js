076bf8daf847d011ab7e6c8b1885ce22326265ff
beezeebug <aelroque.semings@gmail.com>
2022-10-20 19:43:18 +0800

    projectpos
    projectpos

---
+++ login/frontend-login/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Login from "./component/Login.js";
+import { BrowserRouter as Router } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <h1>WELCOME PAGE</h1>
+        <Login />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
