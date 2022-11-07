e001a2bc9c3ef30553adc0741ea41d94ad35aab2
beezeebug <aelroque.semings@gmail.com>
2022-11-07 15:28:54 +0800

    backend /config/allowedorigin

---
+++ backend/config/allowedOrigins.js
@@ -0,0 +1,8 @@
+
+const allowedOrigins = [
+    'http://www.yoursite.com',
+    'http://127.0.0.1:5500',
+    'http://localhost:8000',
+]
+export default allowedOrigins;
+//module.exports = allowedOrigins;
