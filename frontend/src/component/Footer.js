8d12a8b7f3e06066628a7f2e919e7dd8faf1fc64
beezeebug <aelroque.semings@gmail.com>
2022-12-14 05:31:11 +0800

    simple footer component

---
+++ frontend/src/component/Footer.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react';
+
+const Footer = () => {
+  const [clockState, setClockState] = useState();
+  const [dateState, setDateState] = useState();
+
+  useEffect(() => {
+    setInterval(() => {
+      const date = new Date();
+      setClockState(date.toLocaleTimeString());
+      setDateState(date.toDateString());
+    }, 1000);
+  }, []);
+
+  return (
+    <footer>
+      <h3 className="text-white text-center">
+        {clockState} {dateState}
+      </h3>
+      <div className="text-white text-center py-3">
+        immature &copy; All rights reserved. afourrs. 2022
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
