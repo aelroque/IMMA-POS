a26e3de2ab3cd89fe8c5abdabc2a7d37ab673887
beezeebug <aelroque.semings@gmail.com>
2022-12-14 12:36:43 +0800

    modified file

--- frontend/src/component/Login.js
+++ frontend/src/component/Login.js
@@ -1,52 +1,95 @@
-import React from "react";
-import "./login.css";
+import React from 'react';
+import axios from 'axios';
+import { Link, Navigate } from 'react-router-dom';
+import { Container, Form, Button } from 'react-bootstrap';
+import { Helmet } from 'react-helmet-async';
+import { useEffect, useState } from 'react';
+// import { toast } from 'react-toastify';
+import './Login.css';
 
 function Login() {
-  <>
-    <div>
-      <div class="container mt-3">
-        <h3>Modal Example</h3>
-        <p>Click on the button to open the modal.</p>
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
-        <button
-          type="button"
-          class="btn btn-primary"
-          data-bs-toggle="modal"
-          data-bs-target="#myModal"
-        >
-          Open modal
-        </button>
-      </div>
+  const [errMsg, setErrMsg] = useState('');
+  const [success, setSuccess] = useState('');
 
-      <div class="modal" id="myModal">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h4 class="modal-title">Modal Heading</h4>
-              <button
-                type="button"
-                class="btn-close"
-                data-bs-dismiss="modal"
-              ></button>
+  useEffect(() => {
+    setErrMsg('');
+  }, [username, password]);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        'http://localhost:8000/login',
+        JSON.stringify({ username, password }),
+        {
+          headers: { 'Content-Type': 'application/json' },
+          //withCredentials: true
+        }
+      );
+      // toast.success('Logged in successfully.');
+      setSuccess(true);
+      setUsername('');
+      setPassword('');
+    } catch (err) {
+      if (!err?.response) {
+        setErrMsg('No Server Response');
+      } else if (err.response?.status === 400) {
+        setErrMsg('Missing Username or Password');
+      } else if (err.response?.status === 401) {
+        setErrMsg('Unauthorized');
+      } else {
+        setErrMsg('Login Failed');
+      }
+    }
+  };
+
+  return (
+    <Container className="login-box">
+      <Helmet>
+        <title>Sign In</title>
+      </Helmet>
+
+      {success ? (
+        <section>
+          {/* <h1>Login Success!</h1> */}
+          <Navigate to="/dashboard" />
+        </section>
+      ) : (
+        <section>
+          <p>{errMsg}</p>
+          <h1>Sign In</h1>
+          <Form onSubmit={submitHandler}>
+            <Form.Group className="user-box" controlId="username">
+              <Form.Label></Form.Label>
+              <Form.Control
+                placeholder="Username"
+                onChange={(e) => setUsername(e.target.value)}
+                required
+              />
+            </Form.Group>
+
+            <Form.Group className="user-box" controlId="password">
+              <Form.Label></Form.Label>
+              <Form.Control
+                placeholder="Password"
+                type="password"
+                required
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </Form.Group>
+            <div className="user-box">
+              <Button type="submit">Sign In</Button>
             </div>
-
-            <div class="modal-body">Modal body..</div>
-
-            <div class="modal-footer">
-              <button
-                type="button"
-                class="btn btn-danger"
-                data-bs-dismiss="modal"
-              >
-                Close
-              </button>
+            <div className="mb-3">
+              New user? <Link to="/register">Register</Link>
             </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    ;
-  </>;
+          </Form>
+        </section>
+      )}
+    </Container>
+  );
 }
-
 export default Login;
