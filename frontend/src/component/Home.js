181134a44a3c96531e27dd546f4b873304795d37
beezeebug <aelroque.semings@gmail.com>
2022-12-14 08:15:42 +0800

    home page

--- frontend/src/component/Home.js
+++ frontend/src/component/Home.js
@@ -1,5 +1,23 @@
-import React from "react";
+import React from 'react';
+import immasmall from '../immasmall.png';
+import '../App.css';
+import { Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
 
-export default function Home() {
-  return <div>Home</div>;
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>immature</p>
+        <Link to="/login">
+          <Button>
+            <img src={immasmall} className="App-logo" alt="logo" />
+          </Button>
+        </Link>
+        <p>Login</p>
+      </header>
+    </div>
+  );
 }
+
+export default Home;
